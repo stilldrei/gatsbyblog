1dbae02db3973f9fc33d36ae62d85a3748387f3f
Andrea Veronica Mendoza <andimendozait@gmail.com>
2019-06-01 21:50:19 +0800

    Change banner

--- config/index.js
+++ config/index.js
@@ -7,9 +7,9 @@ module.exports = {
   siteUrl: 'https://stilldrei.netlify.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteHeadline: 'Writing and publishing content', // Headline for schema.org JSONLD
-  siteBanner: '/social/banner.jpg', // Your image for og:image tag. You can find it in the /static folder
+  siteBanner: '/social/stilldrei.jpg', // Your image for og:image tag. You can find it in the /static folder
   favicon: 'src/assets/images/dreilogo.png', // Your image for favicons. You can find it in the /src folder
-  siteDescription: 'Minimal Blog with a focus on typography. Gatsby Starter powered by MDX.', // Your site description
+  siteDescription: 'My blog site to post everyday experiences.', // Your site description
   // author: 'LekoArts', // Author for schemaORGJSONLD
   siteLogo: '/social/dreilogo.png', // Image for schemaORGJSONLD
 
